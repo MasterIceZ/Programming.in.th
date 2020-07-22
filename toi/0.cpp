6074a6e0358b61443c155f1d3e6fbf107805ab38
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2020-07-22 10:00:41 +0700

    Copy Temaplate

---
+++ toi/0.cpp
@@ -0,0 +1,25 @@
+/*
+ *	AUTHOR : IceBorworntat
+ * */
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define us unsigned
+#define f0(a,b,c) for(a=b;b<c;a++)
+#define f1(a,b,c) for(a=b;b<=c;a++)
+#define f2(a,b,c) for(a=b;a>c;a--)
+#define f3(a,b,c) for(a=b;a>=c;a--)
+#define r4i {-1,1,0,0}
+#define r4j {0,0,-1,1}
+#define r8i {-1,-1,-1,0,0,1,1,1}
+#define r8j {-1,0,1,-1,1,-1,0,1}
+int i;
+
+int main ()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+
+	return 0;
+}
