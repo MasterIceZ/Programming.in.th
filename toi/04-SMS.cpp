290a4381d399175e698a7dfc05fd3131ce72bf21
Borworntat Dendumrongkul <mastericez890@gmail.com>
2020-08-04 20:33:28 +0700

    toi4_SMS_Thumb

--- toi/04-SMS.cpp
+++ toi/04-SMS.cpp
@@ -14,24 +14,39 @@ using namespace std;
 #define r8i {-1,-1,-1,0,0,1,1,1}
 #define r8j {-1,0,1,-1,1,-1,0,1}
 
-void sms (int pos,int time,int state)
+char a[110];
+char thumb[10][10] = { "ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};
+int now;
+
+void sms (int pos,int time)
 {
 	int i,len;
-	switch(pos)
+	
+	if(pos==1)
 	{
-		case '1' :
-			f0(i,0,time)
+		f0(i,0,time)
+		{
+			len = strlen(a);
+			if(len==0)
 			{
-				len = strlen(a);
-				if(len==0)
-				{
-					return;
-				}
-				a[len-1] = '\0';
-				state--;
+				return ;
 			}
-			break;
+			a[now--] = '\0';
+		}
 	}
+	else
+	{
+		if(pos==7 || pos==9)
+		{
+			time %= 4;
+		}
+		else
+		{
+			time %= 3;
+		}
+		a[now++] = thumb[pos-2][time-1];
+	}
+
 }
 
 int main ()
@@ -39,25 +54,26 @@ int main ()
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int q,pos,time,now,len,changex,changey;
+	int q,pos,time,len,changex,changey;
 	cin >> q;
-	cin >> pos,time;
-	sms(pos,time,now);
+	cin >> pos >> time;
+	now = 0;
+	sms(pos,time);
 	q--;
 	while(q--)
 	{
 		cin >> changex >> changey >> time;
 		pos = pos + changex + (changey * 3);
-		play (pos,time,now);
+		sms(pos,time);
 	}
 	len = strlen(a);
 	if(len == 0)
 	{
-		cout << "null";
+		cout << "null" << endl;
 	}
 	else
 	{
-		cout << a;
+		cout << a << endl;
 	}
 	return 0;
 }
