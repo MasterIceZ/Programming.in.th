b4db74af849e0723bffb559b03a95bb754b7d8c3
Borworntat Dendumrongkul <mastericez890@gmail.com>
2020-08-11 12:59:14 +0700

    ABC

---
+++ 00_Series/ABC.cpp
@@ -0,0 +1,84 @@
+/*
+ *	AUTHOR : IceBorworntat
+ * */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define us unsigned
+#define pii pair<int,int>
+#define f0(a,b,c) for(a=b;b<c;a++)
+#define f1(a,b,c) for(a=b;b<=c;a++)
+#define f2(a,b,c) for(a=b;a>c;a--)
+#define f3(a,b,c) for(a=b;a>=c;a--)
+#define r4i {-1,1,0,0}
+#define r4j {0,0,-1,1}
+#define r8i {-1,-1,-1,0,0,1,1,1}
+#define r8j {-1,0,1,-1,1,-1,0,1}
+#define INF 1e9
+#define arr array
+#define MOD 1e9+7
+#define str string
+#define st struct
+#define def void
+#define qi queue<int>
+#define pb(a) push_back(a)
+#define pq priority_queue
+#define vt vector
+#define vi vector<int>
+#define infile() freopen("input.in","r",stdin)
+#define outfile() freopen("sol.out","w",stdout)
+#define sz(x) (int)(x).size()
+#define all(c) (c).begin(),(c).end()
+#define in cin
+#define out cout
+#define ld long double
+
+const int d4i[] = r4i,d4j[] = r4j;
+const int d8i[] = r8i,d8j[] = r8j;
+
+int a[5];
+char b[5];
+
+void solution()
+{
+	int i;
+	for(i=0;i<3;i++)
+	{
+		cin >> a[i];
+	}
+	sort(a,a+3);
+	cin >> b;
+	for(i=0;i<3;i++)
+	{
+		if(b[i]=='A')
+		{
+			cout << a[0];
+		}	
+		else if(b[i]=='B')
+		{
+			cout << a[1];
+		}
+		else
+		{
+			cout << a[2];
+		}
+		cout << " ";
+	}
+	return ;
+}
+
+int main ()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int q;
+	//cin >> q;
+	q = 1;
+	while(q--)
+	{
+		solution();
+	}
+
+	return 0;
+}
