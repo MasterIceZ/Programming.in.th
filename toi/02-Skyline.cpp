b849fd11edcd7f65f0af522bcdb415f749597baa
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2020-11-11 17:41:23 +0700

    100

--- toi/02-Skyline.cpp
+++ toi/02-Skyline.cpp
@@ -1,80 +1,98 @@
-/*
- *	AUTHOR : IceBorworntat
- * */
 #include <bits/stdc++.h>
 using namespace std;
+#define endl '\n'
+#define space " "
+#define int long long
+#define double long double
+#define sim template<typename T
+#define fi first
+#define se second
 
-#define ll long long
-#define us unsigned
 #define pii pair<int,int>
-#define f0(a,b,c) for(a=b;b<c;a++)
-#define f1(a,b,c) for(a=b;b<=c;a++)
-#define f2(a,b,c) for(a=b;a>c;a--)
-#define f3(a,b,c) for(a=b;a>=c;a--)
-#define r4i {-1,1,0,0}
-#define r4j {0,0,-1,1}
-#define r8i {-1,-1,-1,0,0,1,1,1}
-#define r8j {-1,0,1,-1,1,-1,0,1}
-#define INF 1e9
-#define arr array
-#define MOD 1e9+7
-#define str string
-#define st struct
-#define def void
-#define qi queue<int>
-#define pb(a) push_back(a)
-#define pq priority_queue
-#define vt vector
-#define vi vector<int>
-#define infile() freopen("input.in","r",stdin)
-#define outfile() freopen("sol.out","w",stdout)
+#define pb push_back
 #define sz(x) (int)(x).size()
-#define all(c) (c).begin(),(c).end()
-#define in cin
-#define out cout
-#define ld long double
+#define all(x) (x).begin(),(x).end()
+#define ub upper_bound
+#define lb lower_bound
+#define dec(_) cout << fixed << setprecision(_)
+#define EACH(a) for(auto x : (a))
 
-const int d4i[] = r4i,d4j[] = r4j;
-const int d8i[] = r8i,d8j[] = r8j;
-
-int dp[300];
-
-void solution ()
-{
-	int i,j;
-	int l,h,r,n;
-	cin >> n;
-	f0(i,0,n)
-	{
-		cin >> l >> h >> r;
-		f0(j,l,r)
-		{
-			dp[j] = max(dp[j],h);
-		}
+sim>void __test(vector<T>v){
+	for(auto x : v){
+		cout << x << " ";
 	}
-	int isPrinted=-1;
-	f1(i,1,255)
-	{
-		if(dp[i]!=isPrinted)
-		{
-			cout << i << " " << dp[i] << " " ;
-			isPrinted = dp[i]; 
+	cout << endl;
+}
+sim>vector<T> dp_qs(vector<T>dp,size_t nax){
+	for(int i=1;i<=nax;++i){
+		dp[i] += dp[i-1];
+	}
+	return dp;
+}
+
+int gcd(int a,int b){
+	if(a==0){
+		return b;
+	} 
+	if(b==0){
+		return a;
+	} 
+	return gcd(b%a,b);
+}
+int lcm(int a,int b){
+	return a*b/gcd(a,b);
+}
+string reverse(string s){
+	int n = s.size(),cnt=0; 
+	string rev = s; 
+	for(int i=n-1;i>=0;--i){
+		rev[cnt++] = s[i];
+	}
+   	return rev;
+}
+void writeCase(int _){
+	cout << "Case #" <<  _ << ": \n"; 
+}
+typedef long long ll;
+bool __Q=0;
+bool __writecase=0;
+int n,m;
+const int INF = 2e18;
+
+void solution(){
+	cin >> n;
+	vector<int>dp(257);
+	for(int i=0;i<n;++i){
+		int l,h,r;
+		cin >> l >> h >> r;
+		for(int j=l;j<r;++j){
+			dp[j] = max(h,dp[j]);
+		}
+	}	
+	int now = 0;
+	for(int i=1;i<=255;++i){
+		if(dp[i] != now){
+			cout << i << space << dp[i] << space;
+			now = dp[i];
 		}
 	}
 	return ;
 }
 
-int main ()
-{
+int32_t main (){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	
-	int q;
-	//cin >> q;
-	q=1;
-	while(q--)
-	{
-		solution();
+	int T=1;
+	int _____=0;
+	if(__Q){
+		cin >> T;
 	}
+	do{
+		if(__writecase){
+			writeCase(_____++);
+		}
+		solution();	
+		cout << '\n';
+	}while(--T);
 	return 0;
 }
